a717abce4e8ccd3a032870ffeba8a5402c70999d
Yoaltzin Xolalpa <96920603+Gytbalam@users.noreply.github.com>
2023-11-11 19:59:45 -0500

    Settings file for alien game
    This file is a complement to the alien game.
    This file contains:
    - Setting class (initializes game appearence and ship speed)

---
+++ settings.py
@@ -0,0 +1,12 @@
+"""This classs is made for all the game settings"""
+
+class Settings:
+
+    def __init__(self):
+        """initilize game settings"""
+        self.screen_width = 1220
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        self.ship_speed = 1.5
+
